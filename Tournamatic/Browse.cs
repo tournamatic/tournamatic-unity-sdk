68f992a1a0b8675ff7cbbb4127f74e5d6212cf9b
Dan Bonab <dbonab@yahoo.ca>
2016-03-11 13:06:58 -0800

    Added a sample plugin dll

---
+++ Tournamatic/Browse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournamatic.Model;
+
+namespace Tournamatic
+{
+    public class Browse
+    {
+        public IList<Tournament> GetAll()
+        {
+            return new List<Tournament> {
+                new Tournament {
+                    Title = "Test 1"
+                }
+            };
+        }
+    }
+}
